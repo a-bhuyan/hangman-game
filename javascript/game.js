f88922a308c053a835e973c26ed527b49658e39d
a-bhuyan <arnob.bhuyan@gmail.com>
2017-06-23 22:02:02 -0500

    first edit

---
+++ javascript/game.js
@@ -0,0 +1,41 @@
+var gameBank= ["blah", "bleh", "bloh", "blab"];
+var testWord = gameBank[Math.floor(Math.random()*4)];
+var arrayTestWord = testWord.split("");
+console.log(arrayTestWord);
+var userCorrectGuesses = 1;
+
+//console.log(testWord);
+var something = userCorrectGuesses[2];
+//console.log(something);
+
+function makeBlankTest(lengthOfWord){
+	console.log(LengthOfWord);
+}
+
+function check(userChar){
+	
+}
+
+
+/*
+var choices = ["rocks","papers", "xylophone","scissors"];
+
+document.onkeyup = function(event) {
+	
+
+	var userChoice = event.key;
+	
+
+
+
+
+	document.getElementById("ties").textContent = ties;
+	document.getElementById("wins").textContent = win;
+	document.getElementById("losses").textContent = loss;
+
+}
+*/
+
+
+
+
